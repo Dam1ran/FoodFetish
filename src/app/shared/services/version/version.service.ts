7450924a5e78357c59f7b2da5512703e29342f6e
Dam1ran <damirandevice@gmail.com>
2026-02-03 19:22:28 +0200

    some cosmetics

--- src/app/shared/services/version/version.service.ts
+++ src/app/shared/services/version/version.service.ts
@@ -12,6 +12,16 @@ export class VersionService {
 
   readonly releases = [
     {
+      version: '1.7.5',
+      date: '03.02.2026',
+      changes: [
+        {
+          description: 'UX and cosmetic improvements.',
+          type: VersionChangeType.Maintenance,
+        },
+      ],
+    },
+    {
       version: '1.7.4',
       date: '03.02.2026',
       changes: [
