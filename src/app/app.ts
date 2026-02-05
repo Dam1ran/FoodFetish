4faddbbb38f3b2bfacccc1452022885690c92e70
Dam1ran <damirandevice@gmail.com>
2026-02-05 17:14:52 +0200

    redirect tryout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { RoutePaths } from './shared/routes/route-paths';
 import { ToastsComponent } from './shared/components/toasts.component';
 
@@ -9,6 +9,15 @@ import { ToastsComponent } from './shared/components/toasts.component';
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
+export class App implements OnInit {
   protected readonly routePaths = RoutePaths;
+  protected readonly router = inject(Router);
+  ngOnInit() {
+    const redirect = sessionStorage['redirect'];
+    if (redirect && redirect !== location.pathname) {
+      delete sessionStorage['redirect'];
+      void this.router.navigate([redirect]);
+      console.log('kek');
+    }
+  }
 }
